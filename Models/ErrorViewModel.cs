b958c8c34ae5f31a1e977c42e865c75e48b7fd19
pedroghiotti <pedro.ghiotti.m@gmail.com>
2025-07-11 09:31:41 -0300

    feat: adicionado projeto mvc base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _071125_Desafio_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
